9fd2ea9c658739abe17bbfc61544b27137ec0207
Ulili Nhaga <3706665+ulili5@users.noreply.github.com>
2022-06-03 22:45:44 -0700

    add AspNetCoreMinimalAPI

--- infra/src/Infra/InfraStack.cs
+++ infra/src/Infra/InfraStack.cs
@@ -1,5 +1,8 @@
 using Amazon.CDK;
 using Constructs;
+using Amazon.CDK.AWS.APIGateway;
+using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.Logs;
 
 namespace Infra
 {
@@ -8,7 +11,63 @@ namespace Infra
         internal InfraStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
 
-            var gwHttpAI
+            var buildOption = new BundlingOptions()
+            {
+                Image = Runtime.DOTNET_6.BundlingImage,
+                User = "root",
+                OutputType = BundlingOutput.ARCHIVED,
+                Command = new string[]{
+               "/bin/sh",
+                "-c",
+                " dotnet tool install -g Amazon.Lambda.Tools"+
+                " && dotnet build"+
+                " && dotnet lambda package --output-package /asset-output/function.zip"
+                }
+            };
+
+            var lambdaFunction = new Function(this, "my-func", new FunctionProps
+            {
+                Runtime = Runtime.DOTNET_6,
+                MemorySize = 1024,
+                LogRetention = RetentionDays.ONE_DAY,
+                Handler = "FunctionThree",
+                Code = Code.FromAsset("../apps/src/FunctionThree/", new Amazon.CDK.AWS.S3.Assets.AssetOptions
+                {
+                    Bundling = buildOption
+                }),
+            });
+
+
+            // var httpApiGtw = new RestApi(this, "demo-gtw", new RestApiProps());
+
+            // var resourceF1 = httpApiGtw.Root.AddResource("functionone", new ResourceOptions
+            // {
+            //     DefaultIntegration = new LambdaIntegration(lambdaFunction)
+            // });
+            // resourceF1.AddMethod("ANY", new LambdaIntegration(lambdaFunction));
+            // resourceF1.AddProxy();
+
+            // var resourceF2 = httpApiGtw.Root.AddResource("functiontwo", new ResourceOptions
+            // {
+            //     DefaultIntegration = new LambdaIntegration(lambdaFunction)
+            // });
+            // resourceF2.AddMethod("ANY", new LambdaIntegration(lambdaFunction));
+            // resourceF2.AddProxy();
+
+            var restAPI = new LambdaRestApi(this, "Endpoint", new LambdaRestApiProps
+            {
+                Handler = lambdaFunction,
+                Proxy = true,
+            });
+
+            var resourceF2 = restAPI.Root.AddResource("functiontwo", new ResourceOptions
+            {
+                DefaultIntegration = new LambdaIntegration(lambdaFunction)
+            });
+            resourceF2.AddMethod("ANY", new LambdaIntegration(lambdaFunction));
+            resourceF2.AddProxy();
+
+
         }
     }
 }
--- readme.md
+++ readme.md
@@ -11,9 +11,9 @@ To recreate this same project structure execute the following commands
     ```bash
     mkdir apps
     cd apps
-    dotnet new lambda.EmptyTopLevelFunction -o .    / --name FunctionOne
-    dotnet new lambda.EmptyTopLevelFunction -o .    / --name FunctionTwo
-    dotnet new serverless.AspNetCoreMinimalAPI -o .    / --name FunctionThree
+    dotnet new lambda.EmptyTopLevelFunction -o ./ --name FunctionOne
+    dotnet new lambda.EmptyTopLevelFunction -o ./ --name FunctionTwo
+    dotnet new serverless.AspNetCoreMinimalAPI -o ./ --name FunctionThree
     cd ..
     ```
 
